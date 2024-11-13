b27c226eaebc78a85e7e4715afa0ceb22778de80
NaamaLugasi <naama@unplugged.com>
2024-11-13 17:04:22 +0200

    create types

---
+++ src/types.ts
@@ -0,0 +1,10 @@
+export type WeatherData = {
+    temperature: number;
+    description: string;
+    humidity: number;
+};
+
+export type LoginRequest = {
+    username: string;
+    password: string;
+};
